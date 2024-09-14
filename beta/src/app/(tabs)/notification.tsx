d3546e64949229fdbf16b2231634e31fbbf60543
Duy Toan <toanb2112016@student.ctu.edu.vn>
2024-09-14 20:51:54 +0700

    Add notification
    Tạo giao diện phần thông báo

--- beta/src/app/(tabs)/notification.tsx
+++ beta/src/app/(tabs)/notification.tsx
@@ -1,11 +1,41 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, Platform, FlatList, Dimensions,TouchableOpacity, Image } from 'react-native'
+import React, { useState } from 'react'
+import { FullWindowOverlay } from 'react-native-screens';
 import Notification from '../components/notification/Notification'
 
 const notification = () => {
   return (
     <View>
-      <Notification/>
+        <View style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          paddingBottom: 20,
+        }}>
+          <Text style={{
+            fontSize:Platform.OS === 'ios' ? 14 : 16,
+            padding : 10,
+          }}>Cập nhật đơn hàng</Text>
+          <Text style={{
+            fontSize:Platform.OS === 'ios' ? 14 : 16,
+            padding : 10,
+            color : 'blue'
+          }}>
+            Đọc tất cả
+          </Text>
+        </View>
+        <FlatList
+        data={[1,2,3,4,5]}
+        renderItem={({item}) => <Notification />}
+        initialNumToRender={5}
+        onEndReached={() => {
+          // Load more data here when the user scrolls to the end
+          // You can implement your data loading logic here
+        }}
+        onEndReachedThreshold={0.1}
+        maxToRenderPerBatch={5}
+        windowSize={5}
+        contentContainerStyle={{ paddingBottom: 200 }} // Add padding to the bottom of the list
+        />
     </View>
   )
 }
